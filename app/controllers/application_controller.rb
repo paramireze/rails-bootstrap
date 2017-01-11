765a7a6364415a9ec22792bf672f1a7fd9f47f80
Paul Ramirez <paramireze@gmail.com>
2017-01-10 18:49:15 -0600

    fun with helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,5 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id])
   end
 
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def error_messages_for(object)
     render(:partial => 'application/error_messages', :locals => {:object => object})
   end
+
+  def user_is_logged_in?
+    !!session[:user_id]
+  end
+
+  def is_admin?
+    logger.info current_user.name
+  end
 end
