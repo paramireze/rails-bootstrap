9b2ce221d5814b4459307e323a15f2065c1df832
Paul Ramirez <paramireze@gmail.com>
2017-01-14 14:23:55 -0600

    created distances table so I can quickly edit and change them

--- db/migrate/20170114160408_create_events.rb
+++ db/migrate/20170114160408_create_events.rb
@@ -1,13 +1,15 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def up
       create_table :events do |t|
+        t.int       "distance_id",    :null => true
         t.int       "number",         :null => true,   :limit => 8
         t.string    "title",          :limit => 250
         t.string    "type",           :null => false
         t.string    "cash",           :null => false,   :default => '$5', :limit => 50
         t.string    "description",    :null => true,    :limit => 2500
         t.string    "startLocation",  :null => true,   :limit => 250
-        t.dateTime  "startTime",      :null => true
+        t.datetime  "startTime",      :null => true
+        t.datetime  "retired",        :null => true
         t.boolean   "publish",        :null => false,   :default => false
         t.timestamps
       end
