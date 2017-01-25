840f1e99c7779353448d9ef556758120e649b23a
Paul Ramirez <paramireze@gmail.com>
2017-01-24 22:21:26 -0600

    added event table

--- db/migrate/20170119040215_create_events.rb
+++ db/migrate/20170119040215_create_events.rb
@@ -1,8 +1,25 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :events do |t|
-
+      t.string    "event_type_id",  :null => true
+      t.integer   "distance_id",    :null => true
+      t.integer   "number",         :null => true
+      t.string    "title",          :limit => 250
+      t.string    "cash",           :null => false,   :default => '$5', :limit => 50
+      t.string    "description",    :null => true,    :limit => 2500
+      t.string    "startLocation",  :null => true
+      t.date      "startDate",      :null => true
+      t.time      "startTime",      :null => true
+      t.datetime  "retire",         :null => true
+      t.boolean   "visible",        :null => false,   :default => false
       t.timestamps
     end
+    add_index("events", "event_type_id")
+    add_index("events", "distance_id")
+
+  end
+
+  def down
+    drop_table :events
   end
 end
