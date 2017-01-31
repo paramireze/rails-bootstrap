1ad38d41606552faa6e0887e3e907cc08234f02d
Paul Ramirez <paramireze@gmail.com>
2017-01-30 22:00:31 -0600

    added visible text field

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -72,6 +72,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :description, :startDate, :distance_id, :startLocation)
+      params.require(:event).permit(:title, :description, :startDate, :distance_id, :startLocation, :visible)
     end
 end
